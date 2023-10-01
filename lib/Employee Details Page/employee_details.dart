4ae189af75d8e597f1884129e89d7c381fab8c0d
Debjit Sarkar <68617086+Debjit0@users.noreply.github.com>
2023-10-01 18:20:14 +0530

    gg

---
+++ lib/Employee Details Page/employee_details.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+// ignore: must_be_immutable
+class EmployeeDetails extends StatefulWidget {
+  EmployeeDetails({super.key, required this.empName, required this.docId});
+  String empName;
+  String docId;
+  @override
+  State<EmployeeDetails> createState() => _EmployeeDetailsState();
+}
+
+class _EmployeeDetailsState extends State<EmployeeDetails> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(widget.empName)),
+    );
+  }
+}
--- lib/Employee Screen/employee_screen.dart
+++ lib/Employee Screen/employee_screen.dart
@@ -2,8 +2,10 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:get/get_navigation/get_navigation.dart';
+import 'package:locumspherelimited_admin/Employee%20Details%20Page/employee_details.dart';
 import 'package:locumspherelimited_admin/Employee%20Screen/components/emp_tile2.dart';
 import 'package:locumspherelimited_admin/Verifty%20Employees/verify_employees.dart';
+import 'package:locumspherelimited_admin/utils/search_text_field.dart';
 
 class EmployeesScreen extends StatefulWidget {
   const EmployeesScreen({super.key});
@@ -15,6 +17,69 @@ class EmployeesScreen extends StatefulWidget {
 class _EmployeesScreenState extends State<EmployeesScreen> {
   CollectionReference userCollection =
       FirebaseFirestore.instance.collection('Users');
+
+  //List<DocumentSnapshot> documents = [];
+  TextEditingController searchController = TextEditingController();
+  String searchText = '';
+  List allResults = [];
+  List resultList = [];
+  late Future resultsLoaded;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    searchController.addListener(_onSearchChanged);
+  }
+
+  _onSearchChanged() {
+    searchResults();
+  }
+
+  searchResults() {
+    var showResults = [];
+    if (searchController.text != "") {
+      for (var emp in allResults) {
+        var name = emp['firstname'].toString().toLowerCase();
+        if (name.contains(searchController.text.toLowerCase())) {
+          showResults.add(emp);
+        }
+      }
+    } else {
+      showResults = List.from(allResults);
+    }
+
+    setState(() {
+      resultList = showResults;
+    });
+  }
+
+  @override
+  void dispose() {
+    searchController.removeListener(_onSearchChanged);
+    searchController.dispose();
+    super.dispose();
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    resultsLoaded = getEmployees();
+  }
+
+  getEmployees() async {
+    var empDocuments = await userCollection
+        .where('isverified', isEqualTo: true)
+        .where('accounttype', isEqualTo: 'employee')
+        .get();
+
+    setState(() {
+      allResults = empDocuments.docs;
+    });
+
+    searchResults();
+    return "done";
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,63 +90,34 @@ class _EmployeesScreenState extends State<EmployeesScreen> {
               onPressed: () {
                 Get.to(VerifyEmployees());
               },
-              icon: Icon(Icons.check))
+              icon: Icon(Icons.check)),
         ],
       ),
-      body: StreamBuilder(
-          stream: userCollection
-              .where('isverified', isEqualTo: true)
-              .where('accounttype', isEqualTo: 'employee')
-              .snapshots(),
-          builder: (context, snapshot) {
-            if (snapshot.hasError) {
-              return Text('Something went wrong');
-            }
-
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return Center(child: CircularProgressIndicator(),);
-            }
-
-            
-            
-
-            return ListView.builder(
-              itemCount: snapshot.data!.docs.length,
+      body: Column(
+        children: [
+          SearchTextField(
+            hintText: "Search",
+            controller: searchController,
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: resultList.length,
               itemBuilder: (context, index) {
-                print(snapshot.data!.docs.length);
                 return GestureDetector(
-                  onTap: () {},
-                  child: EmpTile2Widget(title: snapshot.data!.docs[index]['firstname'], subtitle: snapshot.data!.docs[index]['gender']),
+                  onTap: () {
+                    Get.to(EmployeeDetails(
+                        empName: resultList[index]['firstname'],
+                        docId: resultList[index]['uid']));
+                  },
+                  child: EmpTile2Widget(
+                      title: resultList[index]['firstname'],
+                      subtitle: resultList[index]['gender']),
                 );
               },
-            );
-          }),
+            ),
+          ),
+        ],
+      ),
     );
-  }
-
-  changeToTrue({@required String? id}) async {
-    print(id);
-    try {
-      await FirebaseFirestore.instance
-          .collection("Users")
-          .doc(id)
-          .update({"isverified": true}).then((value) => Get.snackbar("Verified", "Employee verified successfully"));
-      print("trued");
-    } catch (e) {
-      print(e);
-    }
-  }
-
-  changeToFalse({@required String? id}) async {
-    print(id);
-    try {
-      await FirebaseFirestore.instance
-          .collection("Users")
-          .doc(id)
-          .update({"isverified": false});
-      print("falsed");
-    } catch (e) {
-      print(e);
-    }
   }
 }
